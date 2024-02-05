ff301f4df48916ed0dbce877349b5bd97f9740db
Jofi <jofi1@seznam.cz>
2024-02-05 20:58:48 +0100

    stavba zakladniho menu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String[] guests = new String[10];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1 - Display All Guests");
+        System.out.println("2 - Add Guest");
+        System.out.println("3 - Remove Guest");
+        System.out.println("Option: ");
+        int option = scanner.nextInt();
+
+        if (option == 1) {
+            for (int i = 0; i < guests.length; i++) {
+                System.out.println(guests[i]);
+            }
+        }
+
     }
 }
