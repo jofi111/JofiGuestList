461379cd32e57cdd024cbc1ed6e7bde86a31d14a
Jofi <jofi1@seznam.cz>
2024-02-05 21:53:50 +0100

    wrap pomoci ctrl alt t / do-while

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String[] guests = new String[10];
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("1 - Display All Guests");
-        System.out.println("2 - Add Guest");
-        System.out.println("3 - Remove Guest");
-        System.out.println("Option: ");
-        int option = scanner.nextInt();
+            String[] guests = new String[10];
+            Scanner scanner = new Scanner(System.in);
 
-        if (option == 1) {
-            for (int i = 0; i < guests.length; i++) {
-                System.out.println(guests[i]);
+        do {
+            System.out.println();
+            System.out.println("1 - Display All Guests");
+            System.out.println("2 - Add Guest");
+            System.out.println("3 - Remove Guest");
+            System.out.println("Option: ");
+            int option = scanner.nextInt();
+            System.out.println();
+
+            if (option == 1) {
+                for (int i = 0; i < guests.length; i++) {
+                    System.out.println(guests[i]);
+                }
             }
-        }
+        } while (true);
 
     }
 }
