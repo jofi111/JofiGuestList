a9fbfda817e343cad3e6c3c6d13af29cffca9764
Jofi <jofi1@seznam.cz>
2024-02-11 16:54:37 +0100

    pridani add guest

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,14 @@ public class Main {
                 for (int i = 0; i < guests.length; i++) {
                     System.out.println(guests[i]);
                 }
-            } else if (option==4) {
+            }
+            else if (option==2) {
+                System.out.println("Name: ");
+                String name = scanner.next();
+                guests[0]=name;
+            }
+
+            else if (option==4) {
                 break;
             }
         } while (true);
