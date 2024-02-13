2f01040e00edc862b496536c1e10bf5bc6f54cc5
Jofi <jofi1@seznam.cz>
2024-02-13 16:03:16 +0100

    automaticke zobrazeni seznamu hostu a menu (i po kazde editaci)

--- src/Main.java
+++ src/Main.java
@@ -3,55 +3,53 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        String[] guests = new String[10];
+        Scanner scanner = new Scanner(System.in);
 
-            String[] guests = new String[10];
-            Scanner scanner = new Scanner(System.in);
+        guests[0] = "Hans";
+        guests[1] = "Marco";
+        guests[2] = "Molly";
+        guests[3] = "Ann";
+        guests[4] = "Fred";
 
-            guests[0]="Hans";
-            guests[1]="Marco";
-            guests[2]="Molly";
-            guests[3]="Ann";
-            guests[4]="Fred";
+        while (true) {
+            System.out.println("_______________________\n- Guests -\n");
+            for (int i = 0; i < guests.length; i++) {
+                System.out.println(guests[i] == null ? "- -" : guests[i]);
+            }
 
-        do {
             System.out.println("_______________________\n- Menu -\n");
-            System.out.println("1 - Display All Guests");
-            System.out.println("2 - Add Guest");
-            System.out.println("3 - Remove Guest");
-            System.out.println("4 - Exit");
+            System.out.println("1 - Add Guest");
+            System.out.println("2 - Remove Guest");
+            System.out.println("3 - Exit");
             System.out.println("Option: ");
+
             int option = scanner.nextInt();
-            System.out.println();
 
             if (option == 1) {
-                System.out.println("_______________________\n- Guests -\n");
-                for (int i = 0; i < guests.length; i++) {
-                    System.out.println(guests[i]==null ? "- -" : guests[i]);
-                }
-            }
-            else if (option==2) {
+                System.out.println("Name: ");
+                String name = scanner.next();
                 for (int i = 0; i < guests.length; i++) {
                     if (guests[i] == null) {
-                        System.out.println("Name: ");
-                        guests[i] = scanner.next();
+                        guests[i] = name;
                         break;
                     }
                 }
-            }
-            else if (option==3) {
+            } else if (option == 2) {
                 System.out.println("Name: ");
-                String name= scanner.next();
+                String name = scanner.next();
                 for (int i = 0; i < guests.length; i++) {
-                    if (guests[i]!=null && guests[i].equals(name)) {
+                    if (guests[i] != null && guests[i].equals(name)) {
                         guests[i] = null;
                         break;
                     }
                 }
-            }
-            else if (option==4) {
+            } else if (option == 3) {
+                System.out.println("Exiting...");
                 break;
+            } else {
+                System.out.println("Invalid option. Please choose 1, 2 or 3.");
             }
-        } while (true);
-        System.out.println("Exiting...");
+        }
     }
 }
